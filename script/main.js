a459b0c4c213583618b53b812efb2c6568216a04
Falk Werner <falk.werner@gmx.net>
2025-11-26 21:53:22 +0100

    allow to join table via url
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ import * as config from "./config.js";
 import { Store } from "./store.js";
 import { Player } from "./player.js";
 import { Lobby } from "./lobby.js";
+import { Tables } from "./tables.js";
 import { TableController } from "./tablecontroller.js";
 
 const BUCKET = 'axOvHLJieLQEvQ-642ng_NQnsNbJENxN';
@@ -18,6 +19,24 @@ async function startup() {
     new Lobby(lobby_elem, store, (table) => {
         join_table(table_controller, table);
     });
+
+    const params = new URLSearchParams(window.location.search);
+    if (params.has("table")) {
+        const name_or_id = params.get("table");
+        const tables = await (new Tables(store)).list();
+        for(const table of tables) {
+            console.log(table);
+            if (table.id == name_or_id) {
+                join_table(table_controller, table);
+                break;
+            }
+            const status = await table.status();
+            if (status.name == name_or_id) {
+                join_table(table_controller, table);
+                break;
+            }
+        }
+    }
 }
 
 async function join_table(table_controller, table) {
